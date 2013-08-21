fd37682295b0545e70372613be60571f25d9dfea
Lauri Takacsi-Nagy <ltakacsi@gmail.com>
2013-08-20 23:20:43 -0700

    ran gofmt

--- problem1/problem1.go
+++ problem1/problem1.go
@@ -3,12 +3,12 @@ package main
 import "fmt"
 
 func main() {
-  sum := 0
-  for i := 0; i < 1000; i++ {
-    if i % 3 == 0 || i % 5 == 0 {
-      sum += i
-    }
-  }
+	sum := 0
+	for i := 0; i < 1000; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
 
-  fmt.Printf("%d\n", sum)
+	fmt.Printf("%d\n", sum)
 }
