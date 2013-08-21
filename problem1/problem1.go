eacfa59916a2a315559822c713138ef511643774
Lauri Takacsi-Nagy <ltakacsi@gmail.com>
2013-08-20 23:40:27 -0700

    made if efficient

--- problem1/problem1.go
+++ problem1/problem1.go
@@ -2,13 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
-	sum := 0
-	for i := 0; i < 1000; i++ {
-		if i%3 == 0 || i%5 == 0 {
-			sum += i
-		}
-	}
+func triangleSum(low int, high int) int {
+	return high*(high+1)/2 - low*(low+1)/2
+}
 
+func divisibleSumLessThan(max int, divisor int) int {
+	return triangleSum(0, (max-1)/divisor) * divisor
+}
+
+func main() {
+	threes_sum := divisibleSumLessThan(1000, 3)
+	fives_sum := divisibleSumLessThan(1000, 5)
+	fifteens_sum := divisibleSumLessThan(1000, 15)
+	sum := threes_sum + fives_sum - fifteens_sum
 	fmt.Printf("%d\n", sum)
 }
