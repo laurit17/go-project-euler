d3687b22817708f85b44596536880f76e3fd2997
Lauri Takacsi-Nagy <ltakacsi@gmail.com>
2013-08-21 22:42:29 -0700

    added library

--- problem1/problem1.go
+++ problem1/problem1.go
@@ -1,19 +1,14 @@
 package main
 
-import "fmt"
-
-func triangleSum(low int, high int) int {
-	return high*(high+1)/2 - low*(low+1)/2
-}
-
-func divisibleSumLessThan(max int, divisor int) int {
-	return triangleSum(0, (max-1)/divisor) * divisor
-}
+import (
+	"fmt"
+	"github.com/laurit17/go_utils/project_euler_utils"
+)
 
 func main() {
-	threes_sum := divisibleSumLessThan(1000, 3)
-	fives_sum := divisibleSumLessThan(1000, 5)
-	fifteens_sum := divisibleSumLessThan(1000, 15)
+	threes_sum := project_euler_utils.DivisibleSumLessThan(1000, 3)
+	fives_sum := project_euler_utils.DivisibleSumLessThan(1000, 5)
+	fifteens_sum := project_euler_utils.DivisibleSumLessThan(1000, 15)
 	sum := threes_sum + fives_sum - fifteens_sum
 	fmt.Printf("%d\n", sum)
 }
