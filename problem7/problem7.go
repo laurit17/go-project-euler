27342cad97915e63154e446e7a459b850903ece6
Lauri Takacsi-Nagy <ltakacsi@gmail.com>
2013-08-26 23:40:52 -0700

    problem 7 done

---
+++ problem7/problem7.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/laurit17/go_utils/iterator_utils"
+)
+
+func sieveOfEratosthenes() func() int {
+	numberStream := iterator_utils.IterStartingFrom(2)
+	return func() int {
+		nextNumber := numberStream()
+		filter_func := func(n int) bool { return n%nextNumber != 0 }
+		numberStream = iterator_utils.FilterIter(filter_func, numberStream)
+		return nextNumber
+	}
+}
+
+func nthPrime(n int) int {
+	iter := sieveOfEratosthenes()
+	for i := 0; i < n-1; i++ {
+		iter()
+	}
+	return iter()
+}
+
+func main() {
+	fmt.Println(nthPrime(10001))
+}
